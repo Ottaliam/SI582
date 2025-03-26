3327c0fb0a2444321be1fef19527aa02d3555c47
Ottaliam <ottaliam@gmail.com>
2025-03-25 23:22:14 -0400

    Add dropdown menu

--- src/TopBar/TopBar.jsx
+++ src/TopBar/TopBar.jsx
@@ -1,26 +1,61 @@
-import {AppBar, Box, IconButton, Toolbar, Typography} from "@mui/material";
+import {useState} from "react";
+
+import {AppBar, Box, IconButton, Menu, MenuItem, Toolbar, Typography} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import FileDownloadIcon from "@mui/icons-material/FileDownload";
+import PageviewIcon from "@mui/icons-material/Pageview";
+import CodeIcon from "@mui/icons-material/Code";
 
 const TopBar = () => {
-  return (
-    <AppBar position="static" color="default" elevation={1}>
-      <Toolbar>
-        <Box sx={{ display: 'flex', gap: 1 }}>
-          <IconButton color="primary">
-            <AddIcon />
-          </IconButton>
-          <IconButton color="primary">
-            <FileDownloadIcon />
-          </IconButton>
-        </Box>
+  const [anchorEl, setAnchorEl] = useState(null);
 
-        <Box sx={{ flexGrow: 1 }} />
-        <Typography variant="h6" component="div">
-          Color Changer
-        </Typography>
-      </Toolbar>
-    </AppBar>
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  }
+
+  return (
+    <>
+      <AppBar position="static" color="default" elevation={1}>
+        <Toolbar>
+          <Box sx={{ display: 'flex', gap: 1 }}>
+            <IconButton color="primary" onClick={handleClick}>
+              <AddIcon />
+            </IconButton>
+            <IconButton color="primary">
+              <FileDownloadIcon />
+            </IconButton>
+          </Box>
+
+          <Box sx={{ flexGrow: 1 }} />
+          <Typography variant="h6" component="div">
+            Color Changer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left'
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <PageviewIcon sx={{ mr: 1 }} />
+          Select Element in Page
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <CodeIcon sx={{ mr: 1 }} />
+          Select with CSS Selector
+        </MenuItem>
+      </Menu>
+    </>
   )
 }
 
